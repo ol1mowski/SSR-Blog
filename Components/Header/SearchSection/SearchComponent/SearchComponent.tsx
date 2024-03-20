010714061803fdb2fc05265058694920390d0533
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-03-20 17:18:28 +0100

    [SearchSectionComponent] refactoring Scss: extract search section scss style to other file

--- Components/Header/SearchSection/SearchComponent/SearchComponent.tsx
+++ Components/Header/SearchSection/SearchComponent/SearchComponent.tsx
@@ -1,13 +1,13 @@
 // import search from "../../../../../assets/search.svg";
 
-import s from "../../Header.module.scss";
+import s from "./SearchSection.module.scss";
 
 const SearchComponent = () => {
   return (
     <>
       <img
         // onClick={showSearchSectionHandler}
-        className={s.header__navMenu__search}
+        className={s.search}
         width="20"
         height="20"
         src="https://img.icons8.com/ios/50/000000/search--v1.png"
@@ -16,11 +16,11 @@ const SearchComponent = () => {
       <section
         // onClick={hideSearchSectionHandler}
         // ref={searchSection}
-        className={s.header__searchSection}
+        className={s.searchSection}
       >
         <div
           // onClick={unBlurryBackgroundHandler}
-          className={s.header__searchSection__searchPlace}
+          className={s.searchSection__searchPlace}
         >
           <input
             // ref={searchCategoryInput}
@@ -28,11 +28,11 @@ const SearchComponent = () => {
             placeholder="Enter category"
             type="text"
             name="search"
-            className={s.header__searchSection__searchPlace__inp}
+            className={s.searchSection__searchPlace__inp}
           />
           <button
             // onClick={categoryClickHandler}
-            className={s.header__searchSection__searchPlace__btn}
+            className={s.searchSection__searchPlace__btn}
           >
             {/* <img width="20" height="20" src={search} alt="search--v1" /> */}
           </button>

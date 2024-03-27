cff2885177c4334530c41b380b78aa2c405323f2
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-03-27 21:30:07 +0100

    [Slider] UI: improve UI

--- Components/Slider/Slider.tsx
+++ Components/Slider/Slider.tsx
@@ -1,36 +1,7 @@
-import Link from "next/link";
-import s from "./Slider.module.scss";
-import Image from "next/image";
+import SliderComponent from "./SliderComponent/SliderContent.component";
 
-import img from "@/assets/react.webp";
-
-const SliderComponent = () => {
-  return (
-    <main className={s.sliderContainer}>
-      <section className={s.sliderContainer__image}>
-        <Image
-          src={img}
-          className={s.sliderContainer__image__img}
-          alt="image"
-          width={400}
-          height={200}
-        />
-      </section>
-      <section className={s.sliderContainer__content}>
-        <h2 className={s.sliderContainer__content__title}>
-          React for Beginners
-        </h2>
-        <p className={s.sliderContainer__content__text}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
-          pariatur corrupti, qui doloremque ipsum tenetur quas optio sed nam
-          laudantium expedita 
-        </p>
-        <Link href={`/`}>
-          <button className={s.sliderContainer__content__read}>Read more</button>
-        </Link>
-      </section>
-    </main>
-  );
+const Slider = () => {
+  return <SliderComponent />;
 };
 
-export default SliderComponent;
+export default Slider;
---
+++ Components/Slider/SliderComponent/SliderContent.component.tsx
@@ -0,0 +1,48 @@
+// import { memo, useEffect, useRef, useState } from "react";
+import s from "./Slider.module.scss";
+
+import img from "@/assets/react.webp";
+import Image from "next/image";
+import Link from "next/link";
+
+const SliderComponent = () => {
+
+  return (
+    <>
+      <main id="home" className={s.sliderContainer}>
+        <div className={s.sliderContainer__image}>
+          <Image
+            src={img}
+            width={250}
+            height={350}
+            draggable="false"
+            alt="post image"
+            className={s.sliderContainer__image__img}
+          />
+        </div>
+        <div className={s.sliderContainer__content}>
+          <p className={s.sliderContainer__content__category}>React</p>
+          <h2 className={s.sliderContainer__content__title}>
+            How to start learn react in 2024 ?
+          </h2>
+          <h3 className={s.sliderContainer__content__text}>
+            Unlock the power of React in 2024! 🚀 Whether you're a beginner or
+            leveling up, this guide will walk you through the essentials. From
+            mastering JSX to building dynamic UIs, embark on your React journey
+            today! 💡💻 #LearnReact #WebDevelopment #CodingGuide
+          </h3>
+          <Link href={`/`}>
+            <p className={s.sliderContainer__content__read}>Read more</p>
+          </Link>
+        </div>
+      </main>
+      <section className={s.sliderContainer__sliderDots}>
+        <div className={s.sliderContainer__sliderDots__dot__active}></div>
+        <div className={s.sliderContainer__sliderDots__dot}></div>
+        <div className={s.sliderContainer__sliderDots__dot}></div>
+      </section>
+    </>
+  );
+};
+
+export default SliderComponent;

053d0060faaf1b8f15f414394db93eab4f572eea
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-01 20:07:35 +0200

    [HomepageComponent] add comtent and class: add content and SCSS class to component

--- Components/Homepage/Homepage.component.tsx
+++ Components/Homepage/Homepage.component.tsx
@@ -1,5 +1,43 @@
+import Image from "next/image";
+import s from "./Homepage.component.module.scss";
+
 const HomepageComponent = () => {
-  return <h1>Main</h1>;
+  return (
+    <section className={s.homepageContainer}>
+      <section className={s.homepageContainer__image}>
+        <div className={s.homepageContainer__image__imageWrapper}>
+          <Image
+            className={s.homepageContainer__image__imageWrapper__img}
+            src={""}
+            alt=""
+          />
+        </div>
+      </section>
+      <section className={s.homepageContainer__content}>
+        <div className={s.homepageContainer__content__title}>
+          <h1>Blog dla Twórców Stron</h1>
+        </div>
+        <div className={s.homepageContainer__content__description}>
+          <p>
+            Znajdziesz tu konkrety o pracy w branży it, Najnowocześniejsze
+            rozwiązania w branży, oraz praktyczne porady i wiele więcej
+          </p>
+        </div>
+        <div className={s.homepageContainer__content__buttonWrapper}>
+          <button
+            className={`${s.homepageContainer__content__buttonWrapper__button} ${s.homepageContainer__content__buttonWrapper__button_read}`}
+          >
+            Czytaj
+          </button>
+          <button
+            className={`${s.homepageContainer__content__buttonWrapper__button} ${s.homepageContainer__content__buttonWrapper__button_find}`}
+          >
+            Znajdź pracę
+          </button>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default HomepageComponent;

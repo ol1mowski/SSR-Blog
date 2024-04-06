7f020c9500aa5e18afa440a698b6474c6fdc25f8
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-06 15:01:25 +0200

    [LastPosts] classes: add classes to last post section

--- Components/LastPosts/LastPosts.component.tsx
+++ Components/LastPosts/LastPosts.component.tsx
@@ -5,22 +5,22 @@ import img from "@/assets/react.webp";
 
 function LastPosts() {
   return (
-    <section>
-      <section>
-        <div>
-          <h2>Najnowsze</h2>
+    <section className={s.lastPostsContainer}>
+      <section className={s.lastPostsContainer__lastPostWrapper}>
+        <div className={s.lastPostsContainer__lastPostContainer__header}>
+          <h2 className={s.lastPostsContainer__lastPostContainer__header__title}>Najnowsze</h2>
         </div>
-        <div>
-          <div>
-            <Image width={250} height={150} src={img} alt="" />
+        <div className={s.lastPostsContainer__lastPostContainer__post}>
+          <div className={s.lastPostsContainer__lastPostContainer__post__imageWrapper}>
+            <Image className={s.lastPostsContainer__lastPostContainer__post__imageWrapper__img} width={250} height={150} src={img} alt="" />
           </div>
-          <div>
-            <h3>Jak zacząć się uczyć React w 2024 roku ?</h3>
-            <p>
+          <div className={s.lastPostsContainer__lastPostContainer__post__content}>
+            <h3 className={s.lastPostsContainer__lastPostContainer__post__content__title}>Jak zacząć się uczyć React w 2024 roku ?</h3>
+            <p className={s.lastPostsContainer__lastPostContainer__post__content__description}>
               Tutaj podzielę się z wami najlepszymi kursami i ksiązkami o
               tematyce tworzenia stron internetowych. Koniecznie musisz je znać
             </p>
-            <span>Oliwier Markiewicz</span>
+            <span className={s.lastPostsContainer__lastPostContainer__post__author}>Oliwier Markiewicz</span>
           </div>
         </div>
       </section>

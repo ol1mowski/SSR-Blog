581bf278470cb808f6a7195a9a4390b7bb63b68b
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-06 16:15:30 +0200

    [LastPosts] add basic layout and styles: add styles to component [!no RWD]

--- Components/LastPosts/LastPosts.component.tsx
+++ Components/LastPosts/LastPosts.component.tsx
@@ -6,21 +6,122 @@ import img from "@/assets/react.webp";
 function LastPosts() {
   return (
     <section className={s.lastPostsContainer}>
+      <div className={s.lastPostsContainer__header}>
+        <h2 className={s.lastPostsContainer__header__title}>Najnowsze</h2>
+      </div>
       <section className={s.lastPostsContainer__lastPostWrapper}>
-        <div className={s.lastPostsContainer__lastPostContainer__header}>
-          <h2 className={s.lastPostsContainer__lastPostContainer__header__title}>Najnowsze</h2>
-        </div>
-        <div className={s.lastPostsContainer__lastPostContainer__post}>
-          <div className={s.lastPostsContainer__lastPostContainer__post__imageWrapper}>
-            <Image className={s.lastPostsContainer__lastPostContainer__post__imageWrapper__img} width={250} height={150} src={img} alt="" />
+        <div className={s.lastPostsContainer__lastPostWrapper__post}>
+          <div
+            className={
+              s.lastPostsContainer__lastPostWrapper__post__imageWrapper
+            }
+          >
+            <Image
+              className={
+                s.lastPostsContainer__lastPostWrapper__post__imageWrapper__img
+              }
+              width={250}
+              height={150}
+              src={img}
+              alt=""
+            />
           </div>
-          <div className={s.lastPostsContainer__lastPostContainer__post__content}>
-            <h3 className={s.lastPostsContainer__lastPostContainer__post__content__title}>Jak zacząć się uczyć React w 2024 roku ?</h3>
-            <p className={s.lastPostsContainer__lastPostContainer__post__content__description}>
+          <div className={s.lastPostsContainer__lastPostWrapper__post__content}>
+            <h3
+              className={
+                s.lastPostsContainer__lastPostWrapper__post__content__title
+              }
+            >
+              Jak zacząć się uczyć React w 2024 roku ?
+            </h3>
+            <p
+              className={
+                s.lastPostsContainer__lastPostWrapper__post__content__description
+              }
+            >
               Tutaj podzielę się z wami najlepszymi kursami i ksiązkami o
               tematyce tworzenia stron internetowych. Koniecznie musisz je znać
             </p>
-            <span className={s.lastPostsContainer__lastPostContainer__post__author}>Oliwier Markiewicz</span>
+            <div
+              className={
+                s.lastPostsContainer__lastPostWrapper__post__content__sub
+              }
+            >
+              <span
+                className={
+                  s.lastPostsContainer__lastPostWrapper__post__content__sub__author
+                }
+              >
+                Oliwier Markiewicz
+              </span>
+              <span
+                className={
+                  s.lastPostsContainer__lastPostWrapper__post__content__sub__author
+                }
+              >
+                05.04.2024
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={s.lastPostsContainer__otherLastPostWrapper}>
+        <div className={s.lastPostsContainer__otherLastPostWrapper__post}>
+          <div
+            className={
+              s.lastPostsContainer__otherLastPostWrapper__post__imageWrapper
+            }
+          >
+            <Image
+              className={
+                s.lastPostsContainer__otherLastPostWrapper__post__imageWrapper__img
+              }
+              width={250}
+              height={150}
+              src={img}
+              alt=""
+            />
+          </div>
+          <div
+            className={
+              s.lastPostsContainer__otherLastPostWrapper__post__content
+            }
+          >
+            <div
+              className={
+                s.lastPostsContainer__otherLastPostWrapper__post__content__category
+              }
+            >
+              React
+            </div>
+            <h3
+              className={
+                s.lastPostsContainer__otherLastPostWrapper__post__content__title
+              }
+            >
+              Jak zacząć się uczyć React w 2024 roku ?
+            </h3>
+
+            <div
+              className={
+                s.lastPostsContainer__otherLastPostWrapper__post__content__sub
+              }
+            >
+              <span
+                className={
+                  s.lastPostsContainer__otherLastPostWrapper__post__content__sub__author
+                }
+              >
+                Oliwier Markiewicz
+              </span>
+              <span
+                className={
+                  s.lastPostsContainer__otherLastPostWrapper__post__content__sub__author
+                }
+              >
+                05.04.2024
+              </span>
+            </div>
           </div>
         </div>
       </section>

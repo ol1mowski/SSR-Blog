d2e7f9c99916a97ecabfe23fa1ce2e5d55c4df2f
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-15 16:21:39 +0100

    [PostsList] animation: introduce animation to content section

--- Components/Posts/Post-Component/PostWrapper.component.tsx
+++ Components/Posts/Post-Component/PostWrapper.component.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { motion  } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import PostImage from "./PostImage/PostImage.component";
 import Post from "./Post/Post.component";
 import { StaticImageData } from "next/image";
 
-import s from './PostWrapper.component.module.scss';
+import s from "./PostWrapper.component.module.scss";
 
 type PostWrapperProps = {
   clock: StaticImageData;
@@ -23,8 +28,28 @@ function PostWrapper({
   data,
   time,
 }: PostWrapperProps) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  const animationVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" },
+    },
+  };
+
   return (
-    <div className={s.postWrapper}>
+    <motion.div
+      className={s.postWrapper}
+      ref={ref}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      variants={animationVariants}
+    >
       <PostImage icon={icon} />
       <Post
         clock={clock}
@@ -34,7 +59,7 @@ function PostWrapper({
         data={data}
         time={time}
       />
-    </div>
+    </motion.div>
   );
 }
 

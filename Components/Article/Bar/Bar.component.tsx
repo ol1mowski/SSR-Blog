a7ad50fbc5a92a0b9ad91ef240d33c05ac15e705
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-18 06:26:33 +0100

    [ArticleSection] BarComponent: create a new component and introducing TSX sketch

---
+++ Components/Article/Bar/Bar.component.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import s from "./Bar.component.module.scss";
+
+import clock from "@/assets/clock.png";
+
+type BarProps = {
+  readTime: number;
+};
+
+function Bar({ readTime }: BarProps) {
+  return (
+    <section className={s.container}>
+      <div className={s.container__categoryWrapper}>
+        <div className={s.container__categoryWrapper__category}>React</div>
+      </div>
+      <div className={s.container__infoWrapper}>
+        <div className={s.container__infoWrapper__date}>04.04.2024</div>
+        <div className={s.container__infoWrapper__readTime}>
+          <Image
+            width={20}
+            height={20}
+            className={s.container__infoWrapper__readTime__img}
+            src={clock}
+            alt=""
+          />
+          <span className={s.container__infoWrapper__readTime__content}>
+            {readTime} min
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Bar;
--- app/[postId]/page.tsx
+++ app/[postId]/page.tsx
@@ -1,11 +1,13 @@
-import Header from "@/Components/Article/Header.component";
+import Header from "@/Components/Article/Header/Header.component";
 
 import s from './page.module.scss';
+import Bar from "@/Components/Article/Bar/Bar.component";
 
 function page({ params }: { params: { postId: string } }) {
   return (
     <main className={s.container}>
       <Header title="Jak zacząć się uczyć React w 2024 roku ? " />
+      <Bar readTime={5}/>
     </main>
   );
 }

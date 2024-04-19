71af197117c312e68b81fc9f549989ed5c5e75ab
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-19 10:06:11 +0100

    [BlogContent] component: introducing blog content component

---
+++ Components/Article/Second-Page/Post-Content/Post-Content.component.tsx
@@ -0,0 +1,9 @@
+import s from './Post-Content.component.module.scss';
+
+function PostContent() {
+  return (
+    <h1>Post</h1>
+  )
+}
+
+export default PostContent
--- app/[postId]/page.tsx
+++ app/[postId]/page.tsx
@@ -1,22 +1,29 @@
-import Header from "@/Components/Article/Header/Header.component";
+import Header from "@/Components/Article/First-Page/Header/Header.component";
 
 import s from "./page.module.scss";
-import Bar from "@/Components/Article/Bar/Bar.component";
+import Bar from "@/Components/Article/First-Page/Bar/Bar.component";
 import img from "@/assets/react.webp";
 
-import ImageComponent from "@/Components/Article/Image/Image.component";
-import AuthorInfo from "@/Components/Article/AuthorInfo/AuthorInfo.component";
+import ImageComponent from "@/Components/Article/First-Page/Image/Image.component";
+import AuthorInfo from "@/Components/Article/First-Page/AuthorInfo/AuthorInfo.component";
+import PostContent from "@/Components/Article/Second-Page/Post-Content/Post-Content.component";
 
 function page({ params }: { params: { postId: string } }) {
   return (
     <main className={s.container}>
-      <section className={s.container__postWrapper}>
-        <Header title="Jak zacząć się uczyć React w 2024 roku ? " />
-        <Bar readTime={5} />
-        <ImageComponent image={img} />
+      <section className={s.container__firstSection}>
+        <section className={s.container__firstSection__postWrapper}>
+          <Header title="Jak zacząć się uczyć React w 2024 roku ? " />
+          <Bar readTime={5} />
+          <ImageComponent image={img} />
+        </section>
+        <section className={s.container__firstSection__authorInfo}>
+          <AuthorInfo />
+        </section>
       </section>
-      <section className={s.container__authorInfo}>
-        <AuthorInfo />
+
+      <section className={s.container__blogSection}>
+        <PostContent />
       </section>
     </main>
   );

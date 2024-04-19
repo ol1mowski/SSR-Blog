44f478baa86f49021f5c819d7d95eed9ea0f157b
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-19 10:17:13 +0100

    [BlogContent] sketch and style: introducing sketch to component and UI style

--- Components/Article/Second-Page/Post-Content/Post-Content.component.tsx
+++ Components/Article/Second-Page/Post-Content/Post-Content.component.tsx
@@ -1,9 +1,23 @@
-import s from './Post-Content.component.module.scss';
+import s from "./Post-Content.component.module.scss";
 
 function PostContent() {
   return (
-    <h1>Post</h1>
-  )
+    <section className={s.container}>
+      <div className={s.container__header}>
+        <h2 className={s.container__header__title}>Co to jest React ?</h2>
+      </div>
+      <div className={s.container__header__content}>
+        React to popularna biblioteka języka JavaScript, stworzona przez
+        inżynierów z Facebooka, która jest używana do budowania interfejsów
+        użytkownika, szczególnie w jednostronicowych aplikacjach (ang.
+        single-page applications). Umożliwia tworzenie dużych aplikacji
+        internetowych, które mogą zmieniać dane wyświetlane na ekranie bez
+        konieczności przeładowywania strony. Główną zaletą Reacta jest skupienie
+        na składnikach (ang. components), co pozwala na łatwe zarządzanie stanem
+        i reużywanie kodu.
+      </div>
+    </section>
+  );
 }
 
-export default PostContent
+export default PostContent;

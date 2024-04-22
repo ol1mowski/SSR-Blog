83d526be750ee7fc70fba06f7f942b2f4234d89f
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-04-22 07:02:40 +0100

    [{first-section} Post-Content] context: introduuce PostVisible context to Post-Component

--- Components/Article/Second-Page/Post-Content/Post-Content.component.tsx
+++ Components/Article/Second-Page/Post-Content/Post-Content.component.tsx
@@ -1,19 +1,57 @@
+"use client";
+import React, { useContext, useEffect, useRef } from "react";
 import s from "./Post-Content.component.module.scss";
+import PostVisibleContext from "@/store/PostVisableContext";
 
 type PostContentProps = {
-  title: string,
-  description: string,
-}
+  title: string;
+  description: string;
+};
 
 function PostContent({ title, description }: PostContentProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { setSectionVisible, sectionVisible } = useContext(PostVisibleContext);
+
+  useEffect(() => {
+    if (ref.current) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              setSectionVisible(title, true);
+            }
+          });
+        },
+        {
+          root: null,
+          rootMargin: "0px",
+          threshold: 0.1,
+        }
+      );
+
+      observer.observe(ref.current);
+
+      return () => {
+        if (ref.current) {
+          observer.unobserve(ref.current);
+        }
+      };
+    } else {
+      setSectionVisible(title, false);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(sectionVisible);
+  }, [sectionVisible]);
+
   return (
-    <section className={s.container}>
+    <section ref={ref} className={s.container}>
       <div className={s.container__header}>
-        <h2 className={s.container__header__title}>{ title }</h2>
+        <h2 className={s.container__header__title}>{title}</h2>
       </div>
-      <div className={s.container__header__content}>
-        { description }
-      </div>
+      <div className={s.container__header__content}>{description}</div>
     </section>
   );
 }
